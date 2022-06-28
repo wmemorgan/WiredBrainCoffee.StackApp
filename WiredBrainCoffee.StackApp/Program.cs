362a4210c7b9eb807b6194bdbe135091342fc3b2
Wilfred Morgan <wmorgan@3cloudsolutions.com>
2022-06-28 13:58:08 -0400

    Add project files.

---
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SimpleStack? stack = new();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
